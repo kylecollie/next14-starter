7ed3cf5c8b6ac16656146c2512e591ce95f76776
Kyle Collie <kcollie@gmail.com>
2024-07-06 13:02:01 -0400

    setup and style homepage

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-
+import styles from './footer.module.css';
 const Footer = () =>
 {
     return (
-        <div>Footer</div>
+        <div className={styles.container}>
+            <div className={styles.logo}>lamadev</div>
+            <div className={styles.text}>
+                Lama creative thoughts agency. ©  All Rights Reserved.
+            </div>
+        </div>
     );
 };
 
