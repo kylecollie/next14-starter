d65008cca84d4c3275d528cd6e6f25650ed516f1
Kyle Collie <kcollie@gmail.com>
2024-07-09 00:12:53 -0400

    WIP

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -10,6 +10,20 @@ const SinglePostPage = () =>
             </div>
             <div className={styles.textContainer}>
                 <h1 className={styles.title}>Single Post</h1>
+                <div className={styles.detail}>
+                    <Image src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="avatar" fill className={styles.avatar} />
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Author</span>
+                        <span className={styles.detailValue}>Terry Jefferson</span>
+                    </div>
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Published</span>
+                        <span className={styles.detailValue}>01.01.2024</span>
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, similique vel vitae libero eum perferendis nam fugiat commodi recusandae voluptates optio sapiente, minus ratione deleniti tempora repellat exercitationem illum ipsum.
+                </div>
             </div>
         </div>
     );
