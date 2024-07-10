5c0ddeb23937507383a25864c2e90ab58903b402
Kyle Collie <kcollie@gmail.com>
2024-07-10 12:18:31 -0400

    WIP

--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import styles from './postCard.module.css';
 import Link from 'next/link';
 
-const PostCard = () =>
+const PostCard = ({ post }) =>
 {
     return (
         <div className={styles.container}>
@@ -13,8 +13,8 @@ const PostCard = () =>
                 <span className={styles.date}>01.01.2024</span>
             </div>
             <div className={styles.bottom}>
-                <h1 className={styles.title}>Title</h1>
-                <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, ullam minus id nesciunt qui natus autem aliquam. Non voluptatum quasi odit reprehenderit inventore dicta. Enim exercitationem optio ex fugiat? Atque?</p>
+                <h1 className={styles.title}>{post.title}</h1>
+                <p className={styles.desc}>{post.body}</p>
                 <Link className={styles.link} href="/blog/post">Read More</Link>
             </div>
         </div>
