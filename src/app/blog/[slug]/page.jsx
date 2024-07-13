e0b1805ead610eb901f3a495ff06d5afbd63243c
Kyle Collie <kcollie@gmail.com>
2024-07-12 20:22:04 -0400

    WIP

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import styles from './singlePost.module.css';
+import PostUser from '@/components/postUser/postUser';
 
 const getData = async (slug) =>
 {
@@ -28,10 +29,7 @@ const SinglePostPage = async ({ params: params }) =>
                 <h1 className={styles.title}>{post.title}</h1>
                 <div className={styles.detail}>
                     <Image src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="avatar" width={50} height={50} className={styles.avatar} />
-                    <div className={styles.detailText}>
-                        <span className={styles.detailTitle}>Author</span>
-                        <span className={styles.detailValue}>Terry Jefferson</span>
-                    </div>
+                    <PostUser userId={post.userId} />
                     <div className={styles.detailText}>
                         <span className={styles.detailTitle}>Published</span>
                         <span className={styles.detailValue}>01.01.2024</span>
