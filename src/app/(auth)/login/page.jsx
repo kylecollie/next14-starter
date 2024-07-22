eaee0009205ccd8b9704d674be7346a81507b778
Kyle Collie <kcollie@gmail.com>
2024-07-22 19:29:51 -0400

    WIP

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,8 +1,22 @@
+import { signIn } from "@/lib/auth";
 
 const LoginPage = () =>
 {
+
+    const handleGithubLogin = async () =>
+    {
+        'use server';
+
+        await signIn('github');
+
+    };
+
     return (
-        <div>LoginPage</div>
+        <div>
+            <form action={handleGithubLogin}>
+                <button>Login with Github</button>
+            </form>
+        </div>
     );
 };
 
